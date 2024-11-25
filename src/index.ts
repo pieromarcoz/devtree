5d758d5af14aed56d77e44fb455ca102f3919979
pierommmg <pmarcosmummagroup@gmail.com>
2024-11-25 13:17:45 -0500

    models

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import server from "./server";
+
+
 const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
 import express from 'express'
 import router from "./router";
-import apiRouter from "./api-router";
 const app = express()
 
 app.use('/', router)
-app.use('/api', apiRouter)
 export default app
